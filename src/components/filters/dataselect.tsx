542d1432064d101e5c52cb0036a2fc46b30e8d0a
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-26 02:33:06 +0300

    Overhauled the website's design to be more consistent

--- src/components/filters/dataselect.tsx
+++ src/components/filters/dataselect.tsx
@@ -2,19 +2,19 @@ function DataSelector() {
     return(
         <div id="dataselector-container" className="panel">
             <div id="gender-selector" className="w-full flex">
-                <label htmlFor="boys"  className="w-2/5 bg-red-400 m-1 p-1 rounded-2xl text-center has-checked:w-3/5 has-checked:bg-green-400 transition-all">
-                    <span>بنين</span>
+                <label htmlFor="boys"  className="radio-selector">
+                    <span className="self-center">بنين</span>
                     <input type="radio" id="boys" name="gender" value="boys" className="hidden" defaultChecked/>
                 </label>
-                <label htmlFor="girls" className="w-2/5 bg-red-400 m-1 p-1 rounded-2xl text-center has-checked:w-3/5 has-checked:bg-green-400 transition-all">
-                    <span>بنات</span>
+                <label htmlFor="girls" className="radio-selector">
+                    <span className="self-center">بنات</span>
                     <input type="radio" id="girls" name="gender" value="girls" className="hidden"/>
                 </label>
             </div>
             <div id="year-selector" className="flex justify-center">
-                <button className="button h-8 w-8">+</button>
-                <span className="h-min self-center mx-8">2025</span>
-                <button className="button h-8 w-8">-</button>
+                <button className="button h-12 w-12">+</button>
+                <span className="h-min self-center mx-8 text-lg">2025</span>
+                <button className="button h-12 w-12">-</button>
             </div>
         </div>
     )
