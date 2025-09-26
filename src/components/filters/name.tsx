cf36c493e16c90dd97a15ee46a08c8eebf606ed0
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-27 00:11:55 +0300

    Added name search and limit search components

--- src/components/filters/name.tsx
+++ src/components/filters/name.tsx
@@ -1,6 +1,21 @@
 function NameSearch() {
     return(
-        <></>
+        <div id="score-search" className="m-1 p-1 flex flex-col">
+            <div id="inputs-container" className="w-full">
+                <div className="m-1">
+                    <label htmlFor="college-name" className="text-center">
+                        <span>اسم الكلية</span>
+                    </label>
+                    <input id="college-name" placeholder="اسم الكلية" type="text" className="w-full h-12"/>
+                </div>
+                <div className="m-1">
+                    <label htmlFor="minimum-score" className="text-center">
+                        <span>الحد الأدنى</span>
+                    </label>
+                    <input id="minimum-score" placeholder="درجة من 0 إلى 410" type="number" min="0" max="410" step="0.000001" className="w-full h-12"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
