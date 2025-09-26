5860d849ce65abc9d50923d1345b7d2396553b57
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-26 14:11:10 +0300

    Added search type selector

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,7 +1,16 @@
 function FiltersSection() {
     return(
         <div id="filters-section" className="panel">
-            filters
+            <div id="searchtype-selector" className="w-full flex">
+                <label htmlFor="score-search"  className="radio-selector !h-16 flex">
+                    <span className="self-center mx-auto">درجتي المدرسة والقدرات</span>
+                    <input type="radio" id="score-search" name="search-type" value="score-search" className="hidden" defaultChecked/>
+                </label>
+                <label htmlFor="name-search" className="radio-selector !h-16 flex">
+                    <span className="self-center mx-auto">اسم الكلية والحد الأدنى</span>
+                    <input type="radio" id="name-search" name="search-type" value="name-search" className="hidden"/>
+                </label>
+            </div>
         </div>
     )
 }
