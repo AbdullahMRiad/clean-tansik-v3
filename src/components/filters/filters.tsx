7b5b1853ef4b545a050fd6e9beec0f08a4b9b3fe
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-27 00:23:26 +0300

    Fixed the height of search type selector being too short at 320px viewport width and made it shorter on wider viewports

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -5,11 +5,11 @@ function FiltersSection() {
     return(
         <div id="filters-section" className="panel">
             <div id="searchtype-selector" className="w-full flex">
-                <label htmlFor="score-search"  className="radio-selector !h-16 !w-1/3 has-checked:!w-2/3 text-center flex">
+                <label htmlFor="score-search"  className="radio-selector !h-20 xs:!h-16 !w-1/3 has-checked:!w-2/3 text-center flex">
                     <span className="self-center mx-auto">درجتي المدرسة والقدرات</span>
                     <input type="radio" id="score-search" name="search-type" value="score-search" className="hidden" defaultChecked/>
                 </label>
-                <label htmlFor="name-search" className="radio-selector !h-16 !w-1/3 has-checked:!w-2/3 text-center flex">
+                <label htmlFor="name-search" className="radio-selector !h-20 xs:!h-16 !w-1/3 has-checked:!w-2/3 text-center flex">
                     <span className="self-center mx-auto">اسم الكلية والحد الأدنى</span>
                     <input type="radio" id="name-search" name="search-type" value="name-search" className="hidden"/>
                 </label>
