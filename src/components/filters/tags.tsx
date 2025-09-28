bafad5e523e2e953b3b877d5957eb62e4e8de4f3
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-28 16:23:40 +0300

    Split tags to major and type

--- src/components/filters/tags.tsx
+++ src/components/filters/tags.tsx
@@ -4,10 +4,20 @@ import { useState } from "react"
 function TagsSection() {
 
     const testArr : any[] = [...Array(15).keys()].map(()=>loremIpsum({count:1,units:"word"}))
+    const testArr2 : string[] = ["كلية", "معهد", "انتساب موجه"]
     return(
-        <div id="tags-section" className="panel">
+        <div id="tags-section" className="panel flex flex-col">
+            <span>المجال:</span>
             <div className="inline">
                 {testArr.map(
+                    (v,i) => (
+                        <Tag key={i}>{v}</Tag>
+                    )
+                )}
+            </div>
+            <span>النوع:</span>
+            <div className="inline">
+                {testArr2.map(
                     (v,i) => (
                         <Tag key={i}>{v}</Tag>
                     )
