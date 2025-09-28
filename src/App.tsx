846eee1c76939be7529db7d7eec98d8775c0f4c5
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-29 01:33:04 +0300

    feat:Added schoolScore, quduratScore, collegeName, limit, and searchType to AppContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react"
 
-import type { College, Gender, Year } from "../types/types"
+import type { College, Gender, SearchType, Year } from "../types/types"
 
 import CardsContainer from "./components/cards"
 import DataSelector from "./components/filters/dataselect"
@@ -17,16 +17,31 @@ export const AppContext = createContext<{
   setYear: React.Dispatch<React.SetStateAction<Year>>
   data: College[]
   setData: React.Dispatch<React.SetStateAction<College[]>>
+  schoolScore: number
+  setSchoolScore: React.Dispatch<React.SetStateAction<number>>
+  quduratScore: number
+  setQuduratScore: React.Dispatch<React.SetStateAction<number>>
+  collegeName: string
+  setCollegeName: React.Dispatch<React.SetStateAction<string>>
+  limit: number
+  setLimit: React.Dispatch<React.SetStateAction<number>>
+  searchType: SearchType
+  setSearchType: React.Dispatch<React.SetStateAction<SearchType>>
 } | undefined>(undefined)
 
 function App() {
 
-  const [gender, setGender] = useState<  Gender >("boys")
-  const [ year ,  setYear ] = useState<   Year  >( 2025 )
-  const [ data ,  setData ] = useState<College[]>(_2025b as College[])
+  const [ gender      , setGender       ] = useState< Gender    >("boys")
+  const [ year        , setYear         ] = useState< Year      >( 2025 )
+  const [ data        , setData         ] = useState< College[] >(_2025b as College[])
+  const [ schoolScore , setSchoolScore  ] = useState< number    >(0)
+  const [ quduratScore, setQuduratScore ] = useState< number    >(0)
+  const [ collegeName , setCollegeName  ] = useState< string    >("")
+  const [ limit       , setLimit        ] = useState< number    >(0)
+  const [ searchType  , setSearchType   ] = useState< SearchType>("score")
 
   return (
-    <AppContext.Provider value={{gender, setGender, year, setYear, data, setData}}>
+    <AppContext.Provider value={{gender, setGender, year, setYear, data, setData, schoolScore, setSchoolScore, quduratScore, setQuduratScore, collegeName, setCollegeName, limit, setLimit, searchType, setSearchType}}>
       <div className="flex flex-col w-full">
         <header className="text-center text-3xl font-bold p-3! panel">تنسيق الجامعات المصرية للشهادة السعودية</header>
         <DataSelector/>
