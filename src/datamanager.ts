b8074ecd66421f846bce39c67b20c221a63bd8f0
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-29 00:38:55 +0300

    Removed datamanager.ts because it's useless

--- src/datamanager.ts
+++
@@ -1,6 +0,0 @@
-import type { Gender } from "../types/types";
-
-export let gender: Gender = "boys"
-export function setGender(genderParam: Gender) {gender = genderParam}
-export let year: number = 2025
-export function setYear(yearParam: number) {year = yearParam}
