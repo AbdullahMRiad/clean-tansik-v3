b04b5e943221d662303b3dfcec1e300338a9f3a8
AbdullahMRiad <abdullahmriad@gmail.com>
2025-09-28 18:01:36 +0300

    Removed unnecessary types and added a gender type

--- src/components/filters/dataselect.tsx
+++ src/components/filters/dataselect.tsx
@@ -1,20 +1,42 @@
+import { useState } from "react"
+import { setGender, setYear, year } from "../../datamanager"
+
 function DataSelector() {
+    const [yearLabel, setYearLabel] = useState(year)
+
     return(
         <div id="dataselector-container" className="panel">
             <div id="gender-selector" className="w-full flex">
                 <label htmlFor="boys"  className="radio-selector flex">
                     <span className="self-center mx-auto">بنين</span>
-                    <input type="radio" id="boys" name="gender" value="boys" className="hidden" defaultChecked/>
+                    <input type="radio" id="boys" name="gender" value="boys" className="hidden" defaultChecked onClick={()=>{setGender("boys")}}/>
                 </label>
                 <label htmlFor="girls" className="radio-selector flex">
                     <span className="self-center mx-auto">بنات</span>
-                    <input type="radio" id="girls" name="gender" value="girls" className="hidden"/>
+                    <input type="radio" id="girls" name="gender" value="girls" className="hidden" onClick={()=>{setGender("girls")}}/>
                 </label>
             </div>
             <div id="year-selector" className="flex justify-center">
-                <button className="button h-12 w-12">+</button>
-                <span className="h-min self-center mx-8 text-lg">2025</span>
-                <button className="button h-12 w-12">-</button>
+                <button
+                    className="button h-12 w-12"
+                    onClick={() => {
+                        if (year < 2025) {
+                            setYear(year + 1);
+                            setYearLabel(year + 1)
+                        }
+                    }}
+                >+
+                </button>
+                <span className="h-min self-center mx-8 text-lg">{yearLabel}</span>
+                <button
+                    className="button h-12 w-12"
+                    onClick={() => {
+                        if (year > 2019) {
+                            setYear(year - 1);
+                            setYearLabel(year - 1)
+                        }
+                    }}
+                >-</button>
             </div>
         </div>
     )
